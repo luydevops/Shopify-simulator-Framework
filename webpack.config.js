ee783268c9cc8a018760e1b58e7a7950e2b2ae77
luydevops <luydevops@example.com>
2025-07-22 11:35:59 -0500

    le di inicio a el test pero tuve que modificar el entorno para realizar aciones atumaticas en el webpack y acondicinar sistema de estilos y js usando sass ademas de reconfigurara el package.json para tener un modo dev para trabajar el test

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,34 @@
+// D:\enviromentDev\Proyects\node\22\TestGradiW\src\webpack.config.js
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './src/app.js',
+  entry: {
+    main: './src/js/app.js',
+    'css/index': './src/scss/index.scss',
+    'css/header/style': './src/scss/header/style.scss',
+  },
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: 'main.js',
+    filename: '[name].js', // genera app.js, index.js, header.js (pero son vacíos en index/header)
+    clean: true, // limpia el folder public antes de compilar
   },
   module: {
     rules: [
       {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: [
+          MiniCssExtractPlugin.loader, // ⚠️ reemplaza style-loader
+          'css-loader',
+          'sass-loader',
+        ],
       },
     ],
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css', // genera app.css, index.css, header.css
+    }),
+  ],
   mode: 'development',
 };
---
+++ webpack.dev.js
@@ -0,0 +1,36 @@
+// D:\enviromentDev\Proyects\node\22\TestGradiW\src\webpack.dev.js
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: {
+        main: './src/js/app.js',
+        'css/index': './src/scss/index.scss',
+        'css/header/style': './src/scss/header/style.scss',
+    },
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: '[name].js', // genera app.js, index.js, header.js (pero son vacíos en index/header)
+        clean: true, // limpia el folder public antes de compilar
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader, // ⚠️ reemplaza style-loader
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].css', // genera app.css, index.css, header.css
+        }),
+    ],
+    mode: 'development',
+    watch: true,
+    devtool: 'source-map',
+};
